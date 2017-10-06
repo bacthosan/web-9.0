338ebc2678ea7c066462ff3dcdb607ead83c62fd
dungph <admiresealsix@gmail.com>
2017-10-07 04:47:29 +0700

    bai 1

--- practice/Practice1/searchPractice.js
+++ practice/Practice1/searchPractice.js
@@ -1,7 +1,13 @@
 'use strict'
-
 function search(input, target) {
-  return input.indexOf(target);  // Remove this line and change to your own algorithm
+  // return input.indexOf(target);  // Remove this line and change to your own algorithm
+  var left = 0, right = target.length;
+  while ( left <= right ) {
+    var mid = ( left + right ) / 2;
+    if ( target[mid] === input ) return mid;
+    if ( target[mid] > input ) right = mid - 1;
+      else left = mid + 1;
+  }
+  return -1;
 }
-
 module.exports = search
