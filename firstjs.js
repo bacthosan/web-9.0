bcbc296934b0daaeb3518cfecea50a9cbb6bb83f
dungph <admiresealsix@gmail.com>
2017-10-07 12:39:13 +0700

    up

--- firstjs.js
+++ firstjs.js
@@ -1,61 +1,72 @@
-var a = 5;
-var b = 6;
-console.log(a+b)
-console.log("Hello world");
-// function Plus(a,b) {
-//     return a + b;
-// }
-var Plus = (a,b) =>  {
-  return a + b;
-}
-function Minus(a,b) {
-  return a - b;
-}
-Minus.property1 = 1;
-var exampleObject =  {
-  objectfuntioin : Plus
-}
-var objectMinusFuntion = Minus;
-console.log('tong a va b ',exampleObject.objectfuntioin(a,b));
-console.log('a - b = ',objectMinusFuntion(a,b));
-console.log(typeof(exampleObject));
-//
-// function writeData() {
-//   console.log(Date.now());
-// }
-// function settimeout(callback) {
-//   setTimeout(function() {
-//     console.log("call back 2");
-//     callback();
-//   },5000);
-//   console.log("set time out done");
-// }
-// settimeout(writeData);
+// // var a = 5;
+// // var b = 6;
+// // console.log(a+b)
+// // console.log("Hello world");
+// // // function Plus(a,b) {
+// // //     return a + b;
+// // // }
+// // var Plus = (a,b) =>  {
+// //   return a + b;
+// // }
+// // function Minus(a,b) {
+// //   return a - b;
+// // }
+// // Minus.property1 = 1;
+// // var exampleObject =  {
+// //   objectfuntioin : Plus
+// // }
+// // var objectMinusFuntion = Minus;
+// // console.log('tong a va b ',exampleObject.objectfuntioin(a,b));
+// // console.log('a - b = ',objectMinusFuntion(a,b));
+// // console.log(typeof(exampleObject));
+// // //
+// // // function writeData() {
+// // //   console.log(Date.now());
+// // // }
+// // // function settimeout(callback) {
+// // //   setTimeout(function() {
+// // //     console.log("call back 2");
+// // //     callback();
+// // //   },5000);
+// // //   console.log("set time out done");
+// // // }
+// // // settimeout(writeData);
+// //
+// // // function countDown(count){
+// // //   for(let i = count; i >= 0; i--){
+// // //     setTimeout(function() {
+// // //       //  write(i);
+// // //       console.log(i);
+// // //     },(count-i)*1000);
+// // //   }
+// // // }
+// // // console.log("count down");
+// // // countDown(6);
+// //
+// // function print2() {
+// //   let b = 6;
+// //   function print() {
+// //     let a = 5;
+// //     console.log(a,b);
+// //   }
+// //   print();
+// // }
+// // print2();
+// // // const print = () => {
+// //   //
+// // // }
+// // // const print = function() {
+// // //
+// // // }
+// //
+// var fs = require("fs");
+// var data = fs.readFileSync('input.txt');
+// console.log("Phuong thuc doc file dong bo: " + data );
+// console.log("Ket thuc chuong trinh");
 
-// function countDown(count){
-//   for(let i = count; i >= 0; i--){
-//     setTimeout(function() {
-//       //  write(i);
-//       console.log(i);
-//     },(count-i)*1000);
-//   }
-// }
-// console.log("count down");
-// countDown(6);
 
-function print2() {
-  let b = 6;
-  function print() {
-    let a = 5;
-    console.log(a,b);
-  }
-  print();
-}
-print2();
-// const print = () => {
-  //
-// }
-// const print = function() {
-//
-// }
-  
+var ta = new Array();
+console.log("array");
+for ( var i = 0; i < 12; i++) ta[i] = Math.floor(Math.random() * 20 );
+  ta.sort(function(a,b){return a-b});
+for ( var i = 0; i < 12; i++) console.log(ta[i]);
