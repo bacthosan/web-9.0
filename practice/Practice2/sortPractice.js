936a2894570fcdf2104be1155e8d279a044c3a1f
dungph <admiresealsix@gmail.com>
2017-10-07 12:35:01 +0700

    abc

--- practice/Practice2/sortPractice.js
+++ practice/Practice2/sortPractice.js
@@ -2,6 +2,14 @@
 
 function sort(input) {
   return input.sort((a,b) => a-b); // Remove this line and change to your own algorithm
+  for ( var i = 0; i < input.length; i++ )
+  for ( var j = i; j < input.lenght; j++ )
+    if ( input[i] > input[j] ) {
+      var tmp = input[i];
+      input[i] = input[j];
+      input[j] = tmp;
+    }
+    return input;
 }
 
 module.exports = sort
