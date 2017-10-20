c52c514d86273fe783651c9accaa583cfe9cd313
dungph <admiresealsix@gmail.com>
2017-10-20 16:51:55 +0700

    btvn router

---
+++ SaaS/editAnswer.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const fileController = require('./fileController');
+const outputFileName = "question.json";
+
+var questionList = new Array();
+var num;
+
+router.get('/',(req,res)=>{
+  num = fileController.readDataFromFile("idQuestion.json");
+  // console.log(num);
+  try {
+    questionList = fileController.readDataFromFile(outputFileName);
+  } catch (ex) {
+    console.log(ex);
+    //questionList = [];
+  }
+  var question = questionList[num];
+  res.render("resultQuestion",{
+    question: question.question,
+    yes: question.yes,
+    no: question.no,
+    layout: "main"
+  });
+})
+router.post('/',(req,res)=>{
+  try {
+    questionList = fileController.readDataFromFile(outputFileName);
+  } catch (ex) {
+    console.log(ex);
+    //questionList = [];
+  }
+
+  let obj = req.body;
+  var t;
+  if ( obj.yes ) {
+    t = obj.yes - '0';
+    questionList[t].yes++;
+  }
+    else {
+      t = obj.no - '0';
+      questionList[t].no++;
+    }
+  fileController.writeDataToFile(outputFileName, questionList);
+  fileController.writeDataToFile("idQuestion.json",t);
+  // console.log(typeof(t));
+  // console.log(t);
+  res.redirect("/resultQuestion");
+})
+module.exports = router;
